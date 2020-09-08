0f4fde93986c3b8544ee971f2591f21354355f8a
Rodrigo-Taveira <rodrigotaveiramelo@gmail.com>
2020-09-08 12:36:03 -0400

    add update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'; //Ultilizado para o render
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
