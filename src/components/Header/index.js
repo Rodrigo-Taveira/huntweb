ac163ced61be1d867ec5b688a11009972bbb9d0a
Rodrigo-Taveira <rodrigotaveiramelo@gmail.com>
2020-09-08 12:33:21 -0400

    finished react-project

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles.css"
+
+const Header = () => (
+    //<header id="main=header">JSHunt!!!!!</header>
+    <div id="main-header">
+        <p>JSHunt</p>
+    </div>
+);
+
+export default Header;
